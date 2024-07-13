67422906bba2f5cc8ace63c53628626cc048eab5
Thomas Schmid <tschmid35@gmail.com>
2024-07-13 18:11:32 +0200

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,16 @@ plugins {
 }
 
 repositories {
+    google()
+    gradlePluginPortal()
     mavenCentral()
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
 }
