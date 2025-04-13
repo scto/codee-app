110c6e4bbb2df9d5b6e7263ce80bb56e9f16c715
scto <tschmid35@gmail.com>
2025-04-13 19:10:46 +0200

    feat: some github workflow additions, gradle fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,8 @@
-plugins {
-  alias(Plugin.ANDROID_APPLICATION) apply false
-  alias(Plugin.ANDROID_LIBRARY) apply false
-  alias(Plugins.KOTLIN_ANDROID) apply false
-  alias(Plugin.KOTLIN) apply false
-  alias(Plugin.JAVA_LIBRARY) apply false
-  alias(Plugin.JVM) apply false
-}
-
-/*
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    /*
     dependencies {
         classpath(androidBuildTools)
         classpath(kotlinGradlePlugin)
     }
-    */
 }
 
 tasks.register<Delete>("clean") { delete(rootProject.layout.buildDirectory) }
