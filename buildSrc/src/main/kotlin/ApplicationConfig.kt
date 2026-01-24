83898563e6d8c628c30380ad472f4aaa2d27f7bb
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-01-24 23:13:57 +0100

    Update ApplicationConfig.kt

--- buildSrc/src/main/kotlin/ApplicationConfig.kt
+++ buildSrc/src/main/kotlin/ApplicationConfig.kt
@@ -4,9 +4,9 @@ object ApplicationConfig {
     const val PACKAGE = "com.codee"
 
     const val APPLICATION_VERSION_INT = 1
-    const val APPLICATION_VERSION_NAME = "1.0.1"
+    const val APPLICATION_VERSION_NAME = "0.0.1"
 
-    const val MIN_SDK_VERSION = 24 // temporary workaround due to error while desugarizing
+    const val MIN_SDK_VERSION = 26 // temporary workaround due to error while desugarizing
     const val TARGET_SDK_VERSION = 34
-    const val BUILD_TOOLS_VERSION = "33.0.3"
+    const val BUILD_TOOLS_VERSION = "34.0.0"
 }
